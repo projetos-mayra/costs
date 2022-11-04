a3fbab54a2bf15478fe9b2297b7eb817f9b10fbd
Mayra Bruno <mayra.brnascimentos@gmail.com>
2022-11-04 10:34:52 -0300

    04/11

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,16 @@
+import styles from './NewProject.module.css'
+
+import ProjectForm from '../project/ProjectForm'
+
+
+function NewProject() {
+    return (
+    <div className={styles.newproject_container}>
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para depois adicionar os serviços</p>
+        <ProjectForm btnText="Criar Projeto" />
+    </div>
+      )
+}
+
+export default NewProject
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,31 @@
+import styles from './ProjectForm.module.css'
+import Select from '../form/Select'
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
+
+
+function ProjectForm({btnText}) {
+    return (
+    <form className={styles.form}>
+     <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+      />
+      <SubmitButton text={btnText} />
+    </form>
+    )
+}
+
+export default ProjectForm
